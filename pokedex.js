c967b61b9fe0ecd716fc4fc59b6d6d607625ad2b
Joui66 <joelamado344@gmail.com>
2023-10-01 17:15:49 -0300

    Cambios en la entrega

---
+++ pokedex.js
@@ -0,0 +1,54 @@
+let containerpokedex1 = document.getElementById("Pokemon1")
+containerpokedex1.innerHTML = `
+    <div>
+        <h3>Bulbasaur</h3>
+        <img src="./img/001.png" alt="">
+        <p>Planta</p>
+        <p>Veneno</p>
+        <button>Ver Detalles</button>
+    </div>`
+console.log(containerpokedex1)
+
+let containerpokedex4 = document.getElementById("Pokemon4")
+containerpokedex4.innerHTML = `
+    <div>
+        <h3>Charmander</h3>
+        <img src="./img/004.png" alt="">
+        <p>Fuego</p>
+        <button>Ver Detalles</button>
+    </div>`
+console.log(containerpokedex4)
+
+let containerpokedex7 = document.getElementById("Pokemon7")
+containerpokedex7.innerHTML = `
+    <div>
+        <h3>Squirtle</h3>
+        <img src="./img/007.png" alt="">
+        <p>Agua</p>
+        <button>Ver Detalles</button>
+    </div>`
+console.log(containerpokedex7)
+
+let containerpokedex10 = document.getElementById("Pokemon10")
+containerpokedex10.innerHTML = `
+    <div>
+        <h3>Caterpie</h3>
+        <img src="./img/010.png" alt="">
+        <p>Bicho</p>
+        <button>Ver Detalles</button>
+    </div>`
+console.log(containerpokedex10)
+
+let containerpokedex25 = document.getElementById("Pokemon25")
+containerpokedex25.innerHTML = `
+    <div>
+        <h3>Pikachu</h3>
+        <img src="./img/025.png" alt="">
+        <p>Eléctrico</p>
+        <button>Ver Detalles</button>
+    </div>`
+console.log(containerpokedex25)
+
+
+
+
